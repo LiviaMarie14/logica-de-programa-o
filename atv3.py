5cd668c35c43fe9943d51c10ee10a9b9f7fe6401
LiviaMarie14 <liviamarievs@gmail.com>
2026-08-07 14:04:51 +0000

    termianndo

---
+++ atv3.py
@@ -0,0 +1,14 @@
+consumo_total = 0
+dias_acima_20 = 0
+
+for dia in range(1, 8):
+    consumo = float(input(f"Informe o consumo do dia {dia} (kWh): "))
+
+    consumo_total += consumo
+
+    if consumo > 20:
+        dias_acima_20 += 1
+
+print("\n===== RELATÓRIO SEMANAL =====")
+print(f"Consumo total da semana: {consumo_total:.2f} kWh")
+print(f"Dias com consumo acima de 20 kWh: {dias_acima_20}")2
